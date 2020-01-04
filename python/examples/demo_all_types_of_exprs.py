bd7b60505cb4aa49af2779cbb2cefce01093a641
Alexey Michurin <a.michurin@gmail.com>
2020-01-04 07:00:23 +0300

    Python: examples: all types of expressions + asserts

--- python/examples/demo_all_types_of_exprs.py
+++ python/examples/demo_all_types_of_exprs.py
@@ -1,9 +1,10 @@
 from milisp import evaluate as E, parse as P
 
 
-def p(env, text):
+def p(env, text, exp):
     res = E(env, P(text))
     print(f'{text} -> {res}')
+    assert res == exp  # slightly naive eq check
 
 
 def op_get_operation_by_name(env, args):
@@ -15,17 +16,17 @@ def op_sum(env, args):
 
 
 def main():
-    p(None, '1')
-    p(None, '"ok"')
-    p(None, '()')
-    p({'x': Ellipsis}, 'x')
-    p({'+': op_sum}, '(+ 1 2)')
+    p(None, '1', 1)
+    p(None, '"ok"', 'ok')
+    p(None, '()', None)
+    p({'x': Ellipsis}, 'x', Ellipsis)
+    p({'+': op_sum}, '(+ 1 2)', 3)
     p({
         'op': op_get_operation_by_name,
         '+': op_sum,
         'x': 1.,
         'y': 2.,
-    }, '((op "+") x y)')
+    }, '((op "+") x y)', 3)
 
 
 if __name__ == '__main__':
